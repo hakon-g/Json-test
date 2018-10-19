88d362fd71ceab776a09c36f48f80bde9f3eaf72
HakonGj <hgjone77@gmail.com>
2018-10-19 23:56:12 +0200

    First commit: Just a simple Hello World.

---
+++ json-converter.py
@@ -0,0 +1,10 @@
+#!usr/bin/env python
+"""json-converter.py, by Haakon Gjone, 2018-10-19
+This program parse a text file and convert it to a json format
+"""
+
+def main():
+    print("Hello world")
+
+if __name__ == "__main__":
+    main()
